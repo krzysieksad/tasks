db0224d07abf9b41ce137aa569fb975e08799a27
krzysieksad <krzysiek.sadlak@gmail.com>
2018-03-12 22:24:15 +0100

    Task 14.3. Fixed.

--- src/main/java/com/crud/tasks/domain/Task.java
+++ src/main/java/com/crud/tasks/domain/Task.java
@@ -1,10 +1,15 @@
 package com.crud.tasks.domain;
 
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Setter(AccessLevel.PRIVATE)
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class Task {
     private Long id;
     private String title;
