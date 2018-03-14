4030495572db6e93a1fd6cfd39604389ff96558f
krzysieksad <krzysiek.sadlak@gmail.com>
2018-03-14 23:09:39 +0100

    Task 14.4. Changes to TaskDao: Added JsonCreator and JsonProperty annotations. Deleted lombok constructors.

--- src/main/java/com/crud/tasks/domain/TaskDto.java
+++ src/main/java/com/crud/tasks/domain/TaskDto.java
@@ -1,14 +1,26 @@
 package com.crud.tasks.domain;
 
-import lombok.AllArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
-@AllArgsConstructor
-@NoArgsConstructor
 public class TaskDto {
     private Long id;
     private String title;
     private String content;
+
+    /**
+     * Create TaskDto.
+     *
+     * @param id      id
+     * @param title   title
+     * @param content content
+     */
+    @JsonCreator
+    public TaskDto(@JsonProperty("id") final Long id, @JsonProperty("title") final String title, @JsonProperty("content") final String content) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+    }
 }
