5e1ff2e29f3a5fa6104e4d3bb80f8181df4bafff
krzysieksad <krzysiek.sadlak@gmail.com>
2018-03-24 18:49:39 +0100

    Task 15.2. Finished.

--- src/main/java/com/crud/tasks/domain/Task.java
+++ src/main/java/com/crud/tasks/domain/Task.java
@@ -6,12 +6,25 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 @Setter(AccessLevel.PRIVATE)
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity(name = "tasks")
 public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @Column(name = "name")
     private String title;
+
+    @Column(name = "description")
     private String content;
 }
---
+++ src/main/java/com/crud/tasks/mapper/TaskMapper.java
@@ -0,0 +1,49 @@
+package com.crud.tasks.mapper;
+
+import com.crud.tasks.domain.Task;
+import com.crud.tasks.domain.TaskDto;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class TaskMapper {
+    /**
+     * Submodule 15.2. mapper TaskDto -> Task.
+     *
+     * @param taskDto TaskDto
+     * @return Task
+     */
+    public Task mapToTask(final TaskDto taskDto) {
+        return new Task(
+                taskDto.getId(),
+                taskDto.getTitle(),
+                taskDto.getContent());
+    }
+
+    /**
+     * Submodule 15.2. mapper Task -> TaskDto.
+     *
+     * @param task Task
+     * @return TaskDto
+     */
+    public TaskDto mapToTaskDto(final Task task) {
+        return new TaskDto(
+                task.getId(),
+                task.getTitle(),
+                task.getContent());
+    }
+
+    /**
+     * Submodule 15.2. mapper for list.
+     *
+     * @param taskList task list
+     * @return List of TaskDto
+     */
+    public List<TaskDto> mapToTaskDtoList(final List<Task> taskList) {
+        return taskList.stream()
+                .map(task -> new TaskDto(task.getId(), task.getTitle(), task.getContent()))
+                .collect(Collectors.toList());
+    }
+}
