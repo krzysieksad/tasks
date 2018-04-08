4e600bc57d12c0d013b3c8b81c9cee916add26b4
krzysieksad <krzysiek.sadlak@gmail.com>
2018-04-09 00:28:22 +0200

    Task 18.2. Finished.

---
+++ src/main/java/com/crud/tasks/trello/client/TrelloClient.java
@@ -0,0 +1,54 @@
+package com.crud.tasks.trello.client;
+
+import com.crud.tasks.controller.TrelloBoardsNotFoundException;
+import com.crud.tasks.domain.TrelloBoardDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class TrelloClient {
+    @Value("${trello.api.endpoint.prod}")
+    private String trelloApiEndpoint;
+    @Value("${trello.app.key}")
+    private String trelloAppKey;
+    @Value("${trello.app.token}")
+    private String trelloToken;
+    @Value("${trello.username}")
+    private String trelloUser;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    /**
+     * Submodule 18.2. Get boards from trello via api.
+     *
+     * @return TrelloBoardDto
+     */
+    public Optional<List<TrelloBoardDto>> getTrelloBoards() {
+
+        TrelloBoardDto[] boardsResponse = restTemplate.getForObject(buildUrl(), TrelloBoardDto[].class);
+
+        /*if (boardsResponse != null) {
+            return Arrays.asList(boardsResponse);
+        }
+        return new ArrayList<>();*/
+
+        return Optional.ofNullable(Arrays.asList(boardsResponse));
+    }
+
+    private URI buildUrl() {
+        return UriComponentsBuilder.fromHttpUrl(trelloApiEndpoint + "/members/" + trelloUser + "/boards")
+                .queryParam("key", trelloAppKey)
+                .queryParam("token", trelloToken)
+                .queryParam("fields", "name,id").build().encode().toUri();
+    }
+}
